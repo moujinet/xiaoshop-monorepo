3544d54879ff4f7a6e9d2c8d7e9ed0045881d1cb
羽白 <luoyi@mouji.net>
2024-04-07 22:34:41 +0800

    feat(admin): 增加 FormPriceInput 组件
    - 优化 CommonPrice 组件

--- packages/admin/src/components/common/common-price.vue
+++ packages/admin/src/components/common/common-price.vue
@@ -3,16 +3,30 @@ defineOptions({
   name: 'CommonPrice',
 })
 
-defineProps<{
+withDefaults(defineProps<{
+  symbol?: string
+
+  mode?: 'prefix' | 'suffix' | 'none'
   price?: number
-}>()
+  fractionDigits?: number
+}>(), {
+  mode: 'none',
+  fractionDigits: 2,
+  icon: true,
+})
 </script>
 
 <template>
-  <span>
-    <slot>
-      {{ price?.toFixed(2) }}
-    </slot>
-    <small c-gray>元</small>
+  <span v-if="price || $slots.default" flex="~ gap-1">
+    <span v-if="symbol">{{ symbol }} </span>
+    <span flex="~ items-baseline gap-0.5">
+      <small v-if="mode === 'prefix'" c-gray>¥</small>
+      <span>
+        <slot>
+          {{ price?.toFixed(fractionDigits) }}
+        </slot>
+      </span>
+      <small v-if="mode === 'suffix'" c-gray>元</small>
+    </span>
   </span>
 </template>
---
+++ packages/admin/src/components/form/form-price-input.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'FormPriceInput',
+})
+
+function formatter(value: string) {
+  const values = value.split('.')
+  values[0] = values[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+  return values.join('.')
+}
+
+function parser(value: string) {
+  return value.replace(/,/g, '')
+}
+</script>
+
+<template>
+  <a-input-number :formatter="formatter" :parser="parser" hide-button>
+    <template #suffix>
+      元
+    </template>
+  </a-input-number>
+</template>
