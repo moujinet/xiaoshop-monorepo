26c834216548947d688bd31c48d25f29d94224fd
羽白 <luoyi@mouji.net>
2024-04-08 15:03:37 +0800

    feat(admin): 增加 FormSelect 组件, 以简化组件传入参数

---
+++ packages/admin/src/components/form/form-select.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'FormSelect',
+})
+</script>
+
+<template>
+  <a-select
+    :field-names="{ label: 'name', value: 'id' }"
+    :default-active-first-option="false"
+    :fallback-option="false"
+    :max-tag-count="3"
+    unmount-on-close
+    allow-clear
+  />
+</template>
