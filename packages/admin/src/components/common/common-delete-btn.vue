ef62dcf7fab784602c82cae2e75afc05fdb712d7
羽白 <luoyi@mouji.net>
2024-04-11 19:47:30 +0800

    feat(admin): 优化 CommonDeleteBtn 组件, 支持改变确认按钮颜色

--- packages/admin/src/components/common/common-delete-btn.vue
+++ packages/admin/src/components/common/common-delete-btn.vue
@@ -9,6 +9,7 @@ defineProps<{
   confirmOkText?: string
   confirmCancelText?: string
   btnText?: string
+  btnType?: 'text' | 'primary' | 'outline' | 'dashed' | 'secondary'
   disabled?: boolean
 }>()
 
@@ -21,11 +22,12 @@ const emit = defineEmits(['delete'])
     :content="confirmContent || '确定要删除吗?'"
     :ok-text="confirmOkText || '删除'"
     :cancel-text="confirmCancelText || '取消'"
+    :ok-button-props="{ status: 'danger' }"
     type="warning"
     @ok="() => emit('delete')"
   >
     <slot>
-      <a-button :disabled="disabled" type="text" status="danger">
+      <a-button :disabled="disabled" :type="btnType || 'text'" status="danger">
         <slot name="text">
           {{ btnText || '删除' }}
         </slot>
