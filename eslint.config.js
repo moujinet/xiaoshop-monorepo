2eedc1dec6aa8b096fcf95528c80c735acdf47a5
羽白 <luoyi@mouji.net>
2024-04-12 13:22:34 +0800

    build: 更新依赖

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  stylistic: {
-    indent: 2,
-    quotes: 'single',
-  },
+export default antfu(
+  {
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+    },
 
-  typescript: true,
-  vue: true,
-})
+    typescript: true,
+
+    vue: true,
+
+    ignores: [
+      'dist',
+      'snapshots*',
+      'node_modules',
+      'packages/apis/**',
+      'packages/docs/**',
+    ],
+  },
+)
