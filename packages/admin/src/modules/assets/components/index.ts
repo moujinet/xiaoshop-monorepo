05e87ad76e707f98ae7706a5e81d2cc7b305f7e1
羽白 <luoyi@mouji.net>
2024-04-14 16:31:21 +0800

    build: 更新依赖

--- packages/admin/src/modules/assets/components/index.ts
+++ packages/admin/src/modules/assets/components/index.ts
@@ -1,6 +1,3 @@
-export { default as AssetsUploader } from './assets-uploader/index.vue'
-export { default as AssetsImage } from './assets-image.vue'
-
 export { default as AssetsBrowser } from './assets-browser/assets-browser.vue'
 export { default as AssetsBrowserImage } from './assets-browser/assets-browser-image.vue'
 export { default as AssetsBrowserLayoutPanel } from './assets-browser/layout/layout-panel.vue'
