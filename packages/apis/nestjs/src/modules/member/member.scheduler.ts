be15508e328fa1ee06af63c6d9ba84159567766c
羽白 <luoyi@mouji.net>
2024-08-06 10:52:21 +0800

    fix(nestjs): `member` 修正

--- packages/apis/nestjs/src/modules/member/member.scheduler.ts
+++ packages/apis/nestjs/src/modules/member/member.scheduler.ts
@@ -25,25 +25,38 @@ export class MemberScheduler {
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleRefreshMemberGroupCount() {
     try {
+      const groups = await this.group.findConditionList()
+
+      if (groups.length > 0) {
+        this.logger.debug('开始刷新会员群体统计')
+
+        for (const group of groups) {
+          this.logger.debug(`开始刷新「${group.name}」会员群体统计`)
+
+          const total = await this.member.countMemberByGroupConditions(group.conditions)
+
+          if (total !== group.total)
+            await this.group.updateTotal(group.id, total)
+
+          this.logger.debug(`刷新「${group.name}」会员群体统计完成, 总数: ${total}`)
+        }
+      }
+    }
+    catch (e) {
+      this.logger.error(e.message)
+    }
+  }
+
+  /**
+   * 注销会员账号 (每小时执行一次)
+   */
+  @Cron(CronExpression.EVERY_HOUR)
+  async handleLogoutMember() {
+    try {
       const enabled = await this.settings.get('member.logout.enableLogout', false)
 
       if (enabled) {
-        const groups = await this.group.findConditionList()
-
-        if (groups.length > 0) {
-          this.logger.debug('开始刷新会员群体统计')
-
-          for (const group of groups) {
-            this.logger.debug(`开始刷新「${group.name}」会员群体统计`)
-
-            const total = await this.member.countMemberByGroupConditions(group.conditions)
-
-            if (total !== group.total)
-              await this.group.updateTotal(group.id, total)
-
-            this.logger.debug(`刷新「${group.name}」会员群体统计完成, 总数: ${total}`)
-          }
-        }
+        this.logger.debug('开始注销会员账号')
       }
     }
     catch (e) {
