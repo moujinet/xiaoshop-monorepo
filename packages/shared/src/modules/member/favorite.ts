ce8b3f03feac71a3cfe8857d2cd186a74c869206
羽白 <luoyi@mouji.net>
2024-11-05 15:37:33 +0800

    types(shared): `member` 更新类型

--- packages/shared/src/modules/member/favorite.ts
+++ packages/shared/src/modules/member/favorite.ts
@@ -1,4 +1,33 @@
 /**
  * 会员收藏商品信息
  */
-export interface IMemberFavoriteInfo {}
+export interface IMemberFavoriteInfo {
+  /**
+   * 收藏 ID
+   */
+  id: number
+  /**
+   * 会员 ID
+   */
+  memberId: number
+  /**
+   * 商品 ID
+   */
+  productId: number
+  /**
+   * 商品名称
+   */
+  name: string
+  /**
+   * 商品图片
+   */
+  image: string
+  /**
+   * 单价
+   */
+  price: number
+  /**
+   * 收藏时间
+   */
+  createdTime: string
+}
--- packages/shared/src/modules/member/history.ts
+++ packages/shared/src/modules/member/history.ts
@@ -1,4 +1,33 @@
 /**
  * 会员商品浏览历史信息
  */
-export interface IMemberHistoryInfo {}
+export interface IMemberHistoryInfo {
+  /**
+   * 浏览 ID
+   */
+  id: number
+  /**
+   * 会员 ID
+   */
+  memberId: number
+  /**
+   * 商品 ID
+   */
+  productId: number
+  /**
+   * 商品名称
+   */
+  name: string
+  /**
+   * 商品图片
+   */
+  image: string
+  /**
+   * 单价
+   */
+  price: number
+  /**
+   * 浏览时间
+   */
+  createdTime: string
+}
